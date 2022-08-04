a1164e32b0fe150f105b41f17082146dc13ededd
Zach Byerley <zachbyerley@gmail.com>
2022-08-03 23:01:50 -0400

    gave head and tail same code as cat, will fix next

--- tail.js
+++ tail.js
@@ -1,3 +1,8 @@
-module.exports = function (callback, arg) {
-    callback(/*TODO*/);
+const fs = require('fs');
+
+module.exports = function (file, callback) {
+    fs.readFile(file, (err, data) => {
+        if (err) throw err;
+        callback(data);
+    });
 };
