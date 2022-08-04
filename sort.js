0efae2f0487b3ee060bc4dbeadd13dd5ac233d00
Zach Byerley <zachbyerley@gmail.com>
2022-08-03 23:34:34 -0400

    implemented sort with bubble sort

--- sort.js
+++ sort.js
@@ -1,3 +1,20 @@
-module.exports = function (callback, arg) {
-    callback(/*TODO*/);
+const fs = require('fs');
+
+module.exports = function (file, callback) {
+    //reads file into buffer object with length 174 for cat.js
+    fs.readFile(file, (err, data) => {
+        if (err) throw err;
+        //need to convert buffer to an array
+        arr = data.toString().split('\n');
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = i; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    let t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                }
+            }
+        }
+        callback(arr.join('\n'));
+    });
 };
