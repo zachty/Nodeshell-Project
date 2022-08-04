6092527de980baa1becfa1d55ad0c92a885fcbc2
Zach Byerley <zachbyerley@gmail.com>
2022-08-03 23:29:52 -0400

    implemented head and tail, bug with extra white space on ends

--- head.js
+++ head.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 
 module.exports = function (file, callback) {
+    //reads file into buffer object with length 174 for cat.js
     fs.readFile(file, (err, data) => {
         if (err) throw err;
-        callback(data);
+        let final = [];
+        //need to convert buffer to an array
+        arr = data.toString().split('\n');
+        for (let i = 0; i < 10; i++) {
+            if (arr[i] === 'undefined') break;
+            final.push(arr[i]);
+        }
+        callback(final.join('\n'));
     });
 };
--- tail.js
+++ tail.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
 module.exports = function (file, callback) {
+    //reads file into buffer object with length 174 for cat.js
     fs.readFile(file, (err, data) => {
         if (err) throw err;
-        callback(data);
+        let final = [];
+        //need to convert buffer to an array
+        arr = data.toString().split('\n');
+        for (let i = 1; i <= 10; i++) {
+            //not sure how else to get rid of white space
+            if (arr[i] === 'undefined') break;
+            final.unshift(arr[arr.length - i]);
+        }
+        callback(final.join('\n'));
     });
 };
